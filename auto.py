f5298f141ad9ac8a836bd16acb77cfb288a957bb
AtomChidjai <atomchidjai@gmail.com>
2025-06-03 13:44:19 +0700

    initialize app script

---
+++ auto.py
@@ -0,0 +1,38 @@
+import requests as req
+import json
+from datetime import datetime
+import seaborn as sns
+import matplotlib.pyplot as plt
+import pandas as pd
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+api_key = os.environ.get('API_KEY')
+url = f'http://api.airvisual.com/v2/city?city=Ban%20Pong&state=Ratchaburi&country=Thailand&key={api_key}'
+res = req.get(url)
+load_text = json.loads(res.text)
+
+date_time = datetime.today().strftime('%Y-%m-%d %H:%M:%S')
+date_filename = datetime.today().strftime('%Y_%m_%d_%H_%M_%S')
+
+air_quality = load_text['data']['current']['pollution']
+weather_info= load_text['data']['current']['weather']
+
+data = {**air_quality, **weather_info}
+
+df = pd.DataFrame([data])
+dfx = df.drop(['ts', 'ic', 'mainus', 'maincn'], axis=1)
+metric = dfx.iloc[0]
+
+df_plot = pd.DataFrame({
+    'Metric': metric.index,
+    'Value': metric.values
+})
+
+plt.figure(figsize=(12, 6))
+sns.barplot(data=df_plot, x='Metric', y='Value')
+plt.title(f'Ban Pong Air Quality & Weather -- {date_time}')
+
+plt.savefig(f'{date_filename}.png')
---
+++ storage.py
@@ -0,0 +1,7 @@
+import boto3
+import os
+import json
+from botocore.client import Config
+from dotenv import load_dotenv
+
+load_dotenv()
